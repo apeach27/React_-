fe5934d5e2aadc978d9f8f2f557211cef557d93b
DH <hyeon@kpopmap.com>
2022-10-07 16:09:53 +0900

    public폴더 이미지 삽입

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
+
+  let [title, setTitle] = useState(['BIG LOGO KNIT PULLOVER IN IVORY', 'handmade wool trench CT', '버켄스탁 보스턴 퍼 모카 1020567'])
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Container fluid>
-          <Navbar.Brand href="#" className='logo'>도도링</Navbar.Brand>
+          <Navbar.Brand href="#" className='logo'>Any Store</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
@@ -43,6 +47,28 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      <div className='main-bg'></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL+'/img/neat1.jpg'} alt="neat" width="80%"/>
+            <h4>{ title[0] }</h4>
+            <p>마뗑킴</p>
+          </div>
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL+'/img/outer1.jpg'} alt="=outer" width="80%"/>
+            <h4>{ title[1] }</h4>
+            <p>링서울</p>
+          </div>
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL+'/img/shoes1.jpg'} alt="shoes" width="80%"/>
+            <h4>{ title[2] }</h4>
+            <p>버켄스탁</p>
+          </div>
+        </div>
+      </div> 
+
 
 
     </div>
