b4ad4dacc39da866d834e319c1c177e2ea1e2145
DH <hyeon@kpopmap.com>
2022-11-07 11:35:00 +0900

    리액트 라우터_navigate, nested routers, outlet

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { useState } from 'react';
 import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom'
-import Detail from './detail.js';
+import { Routes, Route, Link, useNavigate, Outlet, UNSAFE_DataRouterStateContext } from 'react-router-dom'
+import Detail from './pages/detail.js';
 
 function App() {
 
   let [items] = useState(data)
-  console.log(items)
+  //console.log(items)
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -24,8 +25,8 @@ function App() {
               navbarScroll
             >
       
-              <Link to={"/"}>홈</Link>
-              <Link to={"/detail"}>상세페이지</Link>
+              <Nav.Link onClick={()=>{ navigate('/') }}>홈</Nav.Link>
+              <Nav.Link onClick={()=>{ navigate('/detail') }}>상세페이지</Nav.Link>
             </Nav>
             <Form className="d-flex">
               <Form.Control
@@ -40,6 +41,7 @@ function App() {
         </Container>
       </Navbar>
 
+    
 
       <Routes>
         <Route path='/' element={
@@ -56,24 +58,34 @@ function App() {
               }
             </div>
           </div> 
-          
           </>
+
         } />
 
-        <Route path='/detail' element={
-          <>
-          <div className="container">
-            <div className="row">
-              <Detail/>
-            </div>
-          </div>
-          </>
-        } />
+        <Route path='/detail' element={<Detail/>} />
+
+        {/* nested routes */}
+        {/* nested routes의 element 보여주는 곳은 <Outlet> */}
+        <Route path='/about' element={<About/>} >
+          <Route path='member' element={<div>Member</div>} />
+          <Route path='location' element={<div>위치정보</div>} />
+        </Route>
+        
+        <Route path='*' element={<div>없는 페이지입니다.</div>} />
+
       </Routes>
 
     </div>
     
   );
+}
+function About(){
+  return (
+    <div>
+      <h4>회사 정보</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 function Card(props){
@@ -87,6 +99,8 @@ function Card(props){
   )
 }
 
-
+// 221107
+// 폴더구조 만들기
+// pages 폴더 생성 --> 페이지.js 다 모아놓을 것임
 
 export default App;
