0aa127503decdbd5b5626be17fd61986371d77c6
DH <hyeon@kpopmap.com>
2022-11-07 17:28:24 +0900

    자료순서 고유 아이디 활용하기

--- shop/src/pages/detail.js
+++ shop/src/pages/detail.js
@@ -4,15 +4,20 @@ function Detail(props){
 
   let {id} = useParams();
 
+  // 자료순서 고유아이디 활용
+  let 찾은상품 = props.items.find(function(a){
+    return a.id==id
+  })
+
   return (
     <>
       <div className="col-md-6">
         <img src={process.env.PUBLIC_URL+'/img/item0.jpg'} alt="Detail_items" width="100%" />
       </div>
       <div className="col-md-6">
-        <h4 className="pt-5">{props.items[id=0].title}</h4>
-        <p>{props.items[id].title}</p>
-        <p>{props.items[id].price}원</p>
+        <h4 className="pt-5">{찾은상품.title}</h4>
+        <p>{찾은상품.title}</p>
+        <p>{찾은상품.price}원</p>
         <button className="btn btn-danger">주문하기</button> 
       </div>
     </>
